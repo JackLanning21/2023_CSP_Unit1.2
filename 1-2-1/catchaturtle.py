947ccabad8d2cc5f04561a1c032442837cb062bd
11335 <jack.lanning@vtsd.com>
2023-11-13 08:41:01 -0500

    example

--- 1-2-1/catchaturtle.py
+++ 1-2-1/catchaturtle.py
@@ -2,12 +2,20 @@
 #-----import statements-----
 import turtle as trtl
 import random as rand
-
+import turtle as score_writer
 
 #-----game configuration----
 turtle_color = "Blue"
 turtle_size = 3
 turtle_shape = "turtle"
+score = 0
+
+score_writer.penup()
+score_writer.goto(300, 300)
+score_writer.pendown()
+score_writer.hideturtle()
+
+font_setup = ("Arial", 20, "normal")
 
 #-----initialize turtle-----
 jack = trtl.Turtle()
@@ -17,12 +25,21 @@ jack.shape(turtle_shape)
 
 #-----game functions--------
 def turtle_clicked(x, y):
-    print("Jack was clicked!")
+    jack.penup()
+    change_position()
+    jack.pendown()
+    jack.showturtle()
+    update_score()
 def change_position():
-    rand.randint(0,400)
-    new_xpos = rand.randint
-    new_ypos = rand.randint
+    rand.randint(-400,400)
+    new_xpos = rand.randint(-400,400)
+    new_ypos = rand.randint(-400,400)
     jack.goto(new_xpos, new_ypos)
+def update_score():
+    global score
+    score += 1
+    score_writer.clear()
+    score_writer.write(score, font=font_setup)
 
 #-----events----------------
 wn = trtl.Screen()
