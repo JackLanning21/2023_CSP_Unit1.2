8bdde40ad2cf0050753b56ddb547bce1100e85cf
11335 <jack.lanning@vtsd.com>
2023-11-14 07:54:24 -0500

    example

--- 1-2-1/catchaturtle.py
+++ 1-2-1/catchaturtle.py
@@ -3,12 +3,14 @@
 import turtle as trtl
 import random as rand
 import turtle as score_writer
+import turtle as counter
 
 #-----game configuration----
 turtle_color = "Blue"
 turtle_size = 3
 turtle_shape = "turtle"
 score = 0
+colors = ["Blue, Yellow, Red, Green, Gray, Black"]
 
 score_writer.penup()
 score_writer.goto(300, 300)
@@ -16,6 +18,17 @@ score_writer.pendown()
 score_writer.hideturtle()
 
 font_setup = ("Arial", 20, "normal")
+
+counter = trtl.Turtle()
+counter.penup()
+counter.goto(-380, 300)
+counter.pendown()
+counter.hideturtle()
+
+font_setup = ("Arial", 20, "normal")
+timer = 3
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
 
 #-----initialize turtle-----
 jack = trtl.Turtle()
@@ -40,8 +53,24 @@ def update_score():
     score += 1
     score_writer.clear()
     score_writer.write(score, font=font_setup)
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+    if timer_up == True:
+        jack.hideturtle()
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval)
+
+
 
 #-----events----------------
 wn = trtl.Screen()
+wn.bgcolor("Yellow")
 jack.onclick(turtle_clicked)
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()
