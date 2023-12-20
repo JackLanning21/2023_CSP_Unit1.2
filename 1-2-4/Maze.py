2a66971ac42f3877481490d6b18f861f07c12819
11335 <jack.lanning@vtsd.com>
2023-12-20 09:23:51 -0500

    Maze Day 2

--- 1-2-4/Maze.py
+++ 1-2-4/Maze.py
@@ -1,4 +1,5 @@
 import turtle as trtl
+import random as rand
 '''
 Import turtle
 x = starting distance
@@ -19,26 +20,45 @@ maze_painter = trtl.Turtle()
 #Create the Spiral
 x = 12
 y = 5
-trtl.left(90)
-trtl.width(6)
-def maze():
-    global x,y
-    for maze in range(25):
-        trtl.forward(x + y)
-        trtl.left(90)
-        y = y + 20
-    trtl.hideturtle()
+i = 0
+door_random = rand.randint(0, 200)
+barrier_random = rand.randint(0, 200)
+maze_painter.left(90)
+maze_painter.width(6)
+maze_painter.speed(200)
+
 def door():
     maze_painter.forward(10)
     maze_painter.penup()
-    maze_painter.forward(x * 2)
+    maze_painter.forward(x * 3)
     maze_painter.pendown()
+
+def barrier():
+    maze_painter.forward(50)
+    maze_painter.left(90)
+    maze_painter.forward(x * 3)
+    maze_painter.back(x * 3)
+    maze_painter.right(90)
+
+def maze():
+    global x,y,i, door_random, barrier_random
+    door_random = rand.randint(x * 2, (y - x * 2))
+    barrier_random = rand.randint(x * 2, (y - x * 2))
+    for maze in range(25):
+        if i < 3:
+            maze_painter.penup()
+        door()
+        barrier()
+        maze_painter.forward(x + y)
+        maze_painter.left(90)
+        y = y + 20
+        i = i + 1
+    maze_painter.hideturtle()
 maze()
-
-
-
 
 wn = trtl.Screen()
 wn.mainloop()
+
+
 
 
