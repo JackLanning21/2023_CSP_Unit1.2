ac9a87980ea43bd3a72369e25d9c96575c41c526
11335 <jack.lanning@vtsd.com>
2024-01-08 10:11:17 -0500

    Tetris Day 2

--- 1-2-5/game.py
+++ 1-2-5/game.py
@@ -1,7 +1,39 @@
 import turtle as trtl
+import random as rand
 
 start = trtl.Turtle()
 
+
 #Create the on screen "Start Game" button
 
-start.write("Press SPACE to play", align='center', font=("Arial", 70, 'Bold'))
+start.write("Press SPACE to play", align='center', font=("Arial", 25, 'bold'))
+start.hideturtle()
+'''
+def Block ():
+
+
+def TShaped():
+
+
+def LShaped():
+
+
+def ReverseL():
+
+
+def Straight():
+
+
+def LeftZigZag():
+
+
+def RightZigZag():
+
+
+def generate():
+'''
+
+wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
+wn.bgpic("tetris_backgroundUP.gif")
+wn.mainloop()
