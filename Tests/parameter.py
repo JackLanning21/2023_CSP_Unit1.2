3cbd648ecb69a716e238b32dda7e91fbc69271f3
11335 <jack.lanning@vtsd.com>
2024-01-25 08:41:14 -0500

    .

---
+++ Tests/parameter.py
@@ -0,0 +1,10 @@
+#Write a method called addSubtract()
+#This method should take a parameter called operation
+#When the method is called, ask the user for two number
+#Do the operation of the parameter to the inputs
+
+def addSubtract(operation):
+    num1 = input("What is the first number")
+    num2 = input("What is the second number")
+
+addSubtract(operation)
